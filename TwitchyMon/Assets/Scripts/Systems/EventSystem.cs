4e01dd57abe0754e78589111e43cecb94807fe05
Dylan Scheuring <dylanscheuring@gmail.com>
2018-10-16 17:08:26 -0400

    Organized Scripts and created BaseMonster.cs and BattleSystem.cs scripts.

---
+++ TwitchyMon/Assets/Scripts/Systems/EventSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventSystem {
+
+    //ADD DELEGATES BASED ON THE TYPE OF FUNCTION YOU NEED AS AN EVENT
+
+    // DELEGATES
+    public delegate void VoidWithNoParams();
+    public delegate bool BoolWithBoolParam(bool _bool);
+
+    // EVENTS
+    public static event VoidWithNoParams TestEvent;
+    public static event VoidWithNoParams BeginBattle;
+
+
+    public static void TriggerTestEvent()
+    {
+        if (TestEvent != null)
+            TestEvent();
+    }
+
+    public static void TriggerBattle()
+    {
+        if (BeginBattle != null)
+            BeginBattle();
+    }
+
+}
